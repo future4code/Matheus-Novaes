04eb3095771b8d41b40b8d3f3a9deca0b8728c1c
Matheus <matheus.novaes998@gmail.com>
2020-12-01 16:41:33 -0300

    exercicios 1 e 2 feitos, 3/4 pendente

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,14 @@
+//exercicio 1
+let idade = alert(Number(prompt('Qual a Sua Idade?')));
+let amigo = alert(Number(prompt('Qual a idade do seu melhor amigo(a)')));
+let compare = idade >= amigo
+alert(`Sua idade é maior do que a do seu melhor amigo? ${compare}`);
+
+//exercicio 2
+let par = Number(prompt('Digite um numero par, porfavor!'))
+let numero = Number(prompt('Digite outro numero, par'))
+let divida = par / numero
+alert(`o numero dividio é ${divida}`)
+
+//exercicio 3
+

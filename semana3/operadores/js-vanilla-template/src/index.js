b3daeaa1cb40b5df3480515e0ebd0e0430c2a1c0
Matheus <matheus.novaes998@gmail.com>
2020-12-02 13:08:18 -0300

    subindo os exercicios da aula 10

--- semana3/operadores/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -1,9 +1,10 @@
 //exercicio 1
 let idade = alert(Number(prompt('Qual a Sua Idade?')));
 let amigo = alert(Number(prompt('Qual a idade do seu melhor amigo(a)')));
-let compare = idade >= amigo
-alert(`Sua idade é maior do que a do seu melhor amigo? ${compare}`);
+console.log("Sua idade é maior do que a do seu amigo?", idade > amigo)
 
+let diferenca = idade - idadeMelhorAmigo
+console.log(diferenca)
 //exercicio 2
 let par = Number(prompt('Digite um numero par, porfavor!'))
 let numero = Number(prompt('Digite outro numero, par'))
@@ -12,3 +13,26 @@ alert(`o numero dividio é ${divida}`)
 
 //exercicio 3
 
+let listaDeTarefas = []
+let tarefa1 = prompt("Já almoçou hoje? ")
+listaDeTarefas.push(tarefa1)
+
+let tarefa2 = prompt("Já tomou agua hoje? ")
+listaDeTarefas.push(tarefa2)
+
+let tarefa3 = prompt("Já saiu para correr? ")
+listaDeTarefas.push(tarefa3)
+
+console.log(listaDeTarefas)
+
+let indice = prompt("Digite o índice da tarefa que você já realizou: ")
+listaDeTarefas.splice(indice)
+
+console.log(listaDeTarefas)
+
+//exercicio 4
+
+let nomeDoUsuario = prompt("Qual o seu nome?")
+let emailDoUsuario = prompt("Qual o seu e-mail?")
+
+console.log("O e-mail ", emailDoUsuario, " foi cadastrado com sucesso. Seja bem-vinda(o), ", nomeDoUsuario)

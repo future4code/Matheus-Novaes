6eb3ac529e702fb47907fc31e2b53621d353c35c
Matheus <matheus.novaes998@gmail.com>
2020-12-09 14:33:42 -0300

    subindo o gabarito da aula de ontem

--- semana4/planejadora/planner/planner/index.js
+++ semana4/planejadora/planner/planner/index.js
@@ -1,12 +1,7 @@
-const criarTarefa = () => {
-    console.log("funcionando")
-    alert("funcionando")
-}
-
-var capturando = " ";
-
-const capturarTarefa = () =>{
-    capturando = document.getElementById('tarefa').value;
-    document.getElementById('domingo').innerHTML = capturando;
-    console.log()   
+function criarTarefa() {
+    let selecao = document.getElementById("dias-semana").value
+    let tarefa = document.getElementById("tarefa").value
+    document.getElementById(selecao).innerHTML += "<p>- " + tarefa + "</p>"
+    document.getElementById("tarefa").value = ""
+    document.getElementById("dias-semana").value = "domingo"
 }

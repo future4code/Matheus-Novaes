19e494f57dc7c16710b85a588b2024bb2c6f0f08
Matheus <matheus.novaes998@gmail.com>
2021-01-12 16:39:03 -0300

    precsio terminar algumas coisas

---
+++ semana7/aula25/labenusers/src/components/listaPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+    class ListaPage extends React.Component{
+        render(){
+            return (
+            
+                <div>
+                    <ul>
+                    <div> Carregando</div>
+                    <div>
+                       <li>
+                            Lista
+                       </li> 
+                    </div>
+                    </ul>
+                                      
+                </div>
+
+            );
+        }
+    }
+
+export  default ListaPage
